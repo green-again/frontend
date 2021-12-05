e8c6cb2cb900897ff98614f80ee9fac5940a29c8
jmean12 <76937994+jmean12@users.noreply.github.com>
2021-12-05 16:28:08 +0900

    create_themes/Styles (#4)

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './themes/theme';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ front-end/src/styles/GlobalStyles.ts
@@ -0,0 +1,21 @@
+import { normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    ${normalize}
+    body{
+        overflow-x: hidden;
+        width: 100%;
+    }
+    *{
+        border: none;
+        outline: none;
+        list-style: none;
+    }
+    button{
+        cursor: pointer;
+    }
+    img{
+        width: 100vw;
+    }  
+`;
